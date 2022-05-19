a6be3093139c06c3093eaf85964ead1aad20fbd2
joseph <jphctolentino@gmail.com>
2022-05-19 16:48:31 +0100

    init

---
+++ bin/index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const envList = require('../envList.json')
+const yargs = require('yargs');
+
+
+const options = yargs
+  .usage("Usage : -p <path>")
+  .option('p', {
+    alias: 'path',
+    describe: 'Config file_path',
+    type: 'string',
+    demandOption: true
+  }).argv;
+
+const configFile = fs.readFileSync(options.path).toString();
+
+const requiredEnvs = configFile.match(/\b[A-Z_]+\b/g);
+
+const output = {
+
+}
+
+requiredEnvs.forEach((env) => {
+  const found = envList[env];
+  output[env] = found
+})
+const string = JSON.stringify(output, null, 2);
+const validEnv = string
+  .replace(/"/g, "")
+  .replace(/: /g, '=')
+  .replace(/,/g, '')
+  .replace(/^\{/, '')
+  .replace(/\}$/, '');
+
+fs.writeFileSync('.env', validEnv)
+
